419ad9f257970c068172e4f1bfa6a9307c329caa
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-09-02 12:29:40 +0700

    add: product migration

---
+++ database/migrations/2023_09_02_113519_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->d('id');
+            $table->string('name');
+            $table->foreignUuid('created_by')->nullable()->constrained('users');
+            $table->text('description')->nullable();
+            $table->decimal('price', 16, 2)->nullable();
+            $table->integer('stock')->default(1);
+            $table->string('image')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
