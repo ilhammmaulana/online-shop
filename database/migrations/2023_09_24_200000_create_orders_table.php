8fbaf719bf04dd5a354b153edb0ae998684620a5
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-09-24 21:56:57 +0700

    add: design database and migration

---
+++ database/migrations/2023_09_24_200000_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->uuid('id')->primary()->index();
+            $table->timestamps();
+            $table->foreignUuid('created_by')->constrained('users');
+            $table->string('status_transaction', 36)->nullable(true);
+            $table->double('total_price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
